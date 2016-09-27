610da211b8823ff2e359174e1076088c3fa491ee
Harshal <harshal23.eri@gmail.com>
2016-09-26 20:53:06 -0700

    email

--- Kaushalyum/src/com/kaushalyum/services/UserService.java
+++ Kaushalyum/src/com/kaushalyum/services/UserService.java
@@ -4,13 +4,18 @@ import java.util.Date;
 import java.util.Properties;
 
 import javax.mail.Authenticator;
+import javax.mail.BodyPart;
 import javax.mail.Message;
 import javax.mail.MessagingException;
+import javax.mail.Multipart;
 import javax.mail.PasswordAuthentication;
 import javax.mail.Session;
 import javax.mail.Transport;
+import javax.mail.internet.AddressException;
 import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeBodyPart;
 import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMultipart;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -39,7 +44,7 @@ public class UserService {
 			userdetail.setAge(userInfo.getAge());
 			userdetail.setContactno(userInfo.getContactno());
 			userDao.insertUser(userdetail);
-			//this.sendMail(userInfo.getEmail());
+			this.sendMail(userInfo.getEmail());
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			throw new Exception(e.getMessage());
@@ -48,83 +53,56 @@ public class UserService {
 	}
 	
 	private void sendMail(String recipientEmail) throws Exception {
-			
-	/*	String PORT = "587";
-			String STARTTLS = "true";
-			String HOST = "localhost";
-			String PASSWORD = "success@123!";
-			String FROM = "kaushalyum@gmail.com";
-			String USER = "sendMail@gmail.com";
-			String to = recipientEmail;
+	
+	    Properties props = System.getProperties();
+	    props.put("mail.smtp.starttls.enable", true); // added this line
+	    props.put("mail.smtp.host", "smtp.gmail.com");
+	    props.put("mail.smtp.user", "kaushalyum@gmail.com");
+	    props.put("mail.smtp.password", "success@123!");
+	    props.put("mail.smtp.port", "587");
+	    props.put("mail.smtp.auth", true);
 
-			System.out.println("Sending mail...");
-		  // Get system properties
-	      Properties properties = System.getProperties();
+	    Session session = Session.getInstance(props,
+	    new javax.mail.Authenticator() {
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication("kaushalyum@gmail.com", "success@123!");
+            }
+        });
+	    
+	    MimeMessage message = new MimeMessage(session);
 
-	      // Setup mail server
-	      properties.setProperty("mail.smtp.host", "10.101.3.229");
-	      //properties.setProperty("mail.smtp.host", HOST)
+	    System.out.println("Port: "+session.getProperty("mail.smtp.port"));
 
-	      // Get the default Session object.
-	      Session session = Session.getDefaultInstance(properties);
+	    // Create the email addresses involved
+	    try {
+	        InternetAddress from = new InternetAddress("username");
+	        message.setSubject("Yes we can");
+	        message.setFrom(from);
+	        message.addRecipients(Message.RecipientType.TO, InternetAddress.parse(recipientEmail));
 
-	      try {
-	         // Create a default MimeMessage object.
-	         MimeMessage message = new MimeMessage(session);
+	        // Create a multi-part to combine the parts
+	        Multipart multipart = new MimeMultipart("alternative");
 
-	         // Set From: header field of the header.
-	         message.setFrom(new InternetAddress(FROM));
+	        // Create your text message part
+	        BodyPart messageBodyPart = new MimeBodyPart();
+	        messageBodyPart.setText("some text to send        d");
 
-	         // Set To: header field of the header.
-	         message.addRecipient(Message.RecipientType.TO, new InternetAddress(to));
+	        // Add the text part to the multipart
+	        multipart.addBodyPart(messageBodyPart);
 
-	         // Set Subject: header field
-	         message.setSubject("This is the Subject Line!");
+	        // Associate multi-part with message
+	        message.setContent(multipart);
 
-	         // Now set the actual message
-	         message.setText("This is actual message");
-
-	         // Send message
-	         Transport.send(message);
-	         System.out.println("Sent message successfully....");
-	      }catch (MessagingException mex) {
-	         mex.printStackTrace();
-	      } */
-		
-	      final String SSL_FACTORY = "javax.net.ssl.SSLSocketFactory";
-	      // Get a Properties object
-	         Properties props = System.getProperties();
-	         props.setProperty("mail.smtp.host", "smtp.gmail.com");
-	         props.setProperty("mail.smtp.socketFactory.class", SSL_FACTORY);
-	         props.setProperty("mail.smtp.socketFactory.fallback", "false");
-	         props.setProperty("mail.smtp.port", "465");
-	         props.setProperty("mail.smtp.socketFactory.port", "465");
-	         props.put("mail.smtp.auth", "true");
-	         props.put("mail.debug", "true");
-	         props.put("mail.store.protocol", "pop3");
-	         props.put("mail.transport.protocol", "smtp");
-	         final String username = "kaushalyum@gmail.com";//
-	         final String password = "success@123!";
-	         try{
-	         Session session = Session.getDefaultInstance(props, 
-	                              new Authenticator(){
-	                                 protected PasswordAuthentication getPasswordAuthentication() {
-	                                    return new PasswordAuthentication(username, password);
-	                                 }});
-
-	       // -- Create a new message --
-	         Message msg = new MimeMessage(session);
-
-	      // -- Set the FROM and TO fields --
-	         msg.setFrom(new InternetAddress("xxxx@gmail.com"));
-	         msg.setRecipients(Message.RecipientType.TO, 
-	                          InternetAddress.parse(recipientEmail,false));
-	         msg.setSubject("Hello");
-	         msg.setText("How are you");
-	         msg.setSentDate(new Date());
-	         Transport.send(msg);
-	         System.out.println("Message sent.");
-	      }catch (MessagingException e){ System.out.println("Erreur d'envoi, cause: " + e);}
-	      }   
-	      
+	        // Send message
+	        Transport transport = session.getTransport("smtp");
+	        transport.connect("smtp.gmail.com", "username", "password");
+	        transport.sendMessage(message, message.getAllRecipients());
+	    } catch (AddressException e) {
+	        // TODO Auto-generated catch block
+	        e.printStackTrace();
+	    } catch (MessagingException e) {
+	        // TODO Auto-generated catch block
+	        e.printStackTrace();
+	    }
+	    }
 }
